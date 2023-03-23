f8cb769a0185692cd747b058e62ad48fd431524a
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-03-23 16:14:18 +0700

    [MOD] logout icon

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,11 +1,13 @@
 import { signOut } from "next-auth/react";
+import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
 
 function Logout() {
   return (
     <button
       onClick={() => signOut()}
-      className="mt-auto flex justify-center hover:bg-red-400/75 hover:text-red-800 text-white hover:font-semibold rounded-lg px-5 py-1 transition-all duration-200 ease-out"
+      className="mt-auto flex justify-center items-center gap-2 hover:bg-red-500/75 hover:ring-red-400 ring-1 ring-gray-700 text-white hover:font-semibold rounded-lg px-5 py-2 transition-all duration-200 ease-out"
     >
+      <ArrowLeftOnRectangleIcon className="w-5 h-5 text-gray-300" />
       Log Out
     </button>
   );
