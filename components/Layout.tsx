e2b81a94b6f19eb0cf67a18b8c83898b5ed669bd
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-03-23 16:13:03 +0700

    [MOD] change background

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import SideBar from "./SideBar";
 import { useSession } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 import Login from "./Login";
 
 interface LayoutProps {
@@ -7,7 +8,6 @@ interface LayoutProps {
 }
 function Layout({ children }: LayoutProps) {
   const { data: session } = useSession();
-  console.log("session => ", session);
 
   if (!session) return <Login />;
 
@@ -16,7 +16,8 @@ function Layout({ children }: LayoutProps) {
       <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
         <SideBar />
       </div>
-      <div className="bg-[#343541] dark:bg-gray-800 flex-1">{children}</div>
+      <Toaster position="top-right" />
+      <div className="bg-[rgb(52,53,65)] flex-1">{children}</div>
     </div>
   );
 }
