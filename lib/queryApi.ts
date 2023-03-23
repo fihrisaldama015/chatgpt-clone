1ec085e21dfbc85bd2f59a2417c3dbb8654645ae
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-03-23 16:07:46 +0700

    [ADD] openai lib

---
+++ lib/queryApi.ts
@@ -0,0 +1,22 @@
+import openai from "./chatgpt";
+
+const query = async (prompt: string, chatId: string, model: string) => {
+  const res = openai
+    .createCompletion({
+      model,
+      prompt,
+      temperature: 0.9,
+      top_p: 1,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    })
+    .then((res) => res.data.choices[0].text)
+    .catch(
+      (err) =>
+        `ChatGPT was unable to generate the answer! (Error: ${err.message})`
+    );
+  return res;
+};
+
+export default query;
