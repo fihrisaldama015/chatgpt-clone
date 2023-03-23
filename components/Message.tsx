445385c037e3b0e67eb856456df21b9cc7704462
Muhamad Fihris Aldama <fihrisaldama015@gmail.com>
2023-03-23 18:34:30 +0700

    [FIX] line break

--- components/Message.tsx
+++ components/Message.tsx
@@ -6,6 +6,10 @@ type MessageProps = {
 
 function Message({ message }: MessageProps) {
   const isChatGPT = message.user._id == "ChatGPT";
+  function replaceAnswer(text: string) {
+    if (text.substring(0, 2) == "\n\n") return text.substring(2, text.length);
+    else return text;
+  }
   return (
     <div className={`py-5 ${isChatGPT ? "bg-gray-600/30" : ""} text-gray-300`}>
       <p className="px-5 text-right text-xs text-gray-300/50 hidden md:block h-min">
@@ -17,7 +21,7 @@ function Message({ message }: MessageProps) {
           alt="profil"
           className="w-8 h-8 rounded-full"
         />
-        <p>{message.text}</p>
+        <p className="whitespace-pre-wrap">{replaceAnswer(message.text)}</p>
       </div>
     </div>
   );
